399b7d1f4082c89f88732ba81505d936656c0c16
Mukka Deepak <ramu92013@gmail.com>
2021-10-03 10:38:43 +0530

    feat: ✨ add routes

---
+++ app.js
@@ -0,0 +1,4 @@
+const {app}=require('./services/fruits')
+app.listen(3000,()=>{
+    console.log("Server is running")
+})
--- services/fruits.js
+++ services/fruits.js
@@ -1,2 +1,16 @@
+const express=require('express')
 const fruits=require('../database/fruits.json')
-console.log(fruits)
+const app=express()
+app.get('/',(req,res)=>{
+    res.json("Welcome to the fruits database")
+})
+
+app.get('/fruits/:fruitname',(req,res)=>{
+    const fruitName=req.params.fruitname
+    const fruitDetails=fruits.filter((fruit)=>{
+           return fruit.name.toLowerCase()===fruitName.toLowerCase()
+    })
+    res.send(fruitDetails)
+})
+
+module.exports.app=app
